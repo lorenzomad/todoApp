3da8aa5a9af082fe6d0bbff23c0d5409f7a78fb8
lorenzo <lorenzo.maddalena1@gmail.com>
2023-06-25 21:29:37 +0100

    cleaned index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,4 @@
 //entry point for the app
-import { Todo } from "./todo";
-import { TodoManager } from "./todoManager";
 import { DisplayManager } from "./displayManager";
 
-
-
-let first = Todo("fare la spesa")
-first.description = 'devo comprare le uova'
-
-let second = Todo('you gotta be kidding bro')
-second.description = 'fatti i cazzi tuoi'
-second.priority = 4
-
-TodoManager.newProject('ciccia')
-TodoManager.addTodo(first, 'ciccia')
-TodoManager.addTodo(second, 'ciccia')
-
-console.log(TodoManager.todoList['ciccia'])
-
-DisplayManager.displayList(TodoManager.todoList['ciccia'])
 DisplayManager.addEvents()
